b3aabea0c693657490fafe5a8c835bb41bb15883
Maksim Ilnitstyi <masimo4work@gmail.com>
2019-06-10 15:50:49 +0300

    point

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -2,9 +2,6 @@ import styled from 'styled-components';
 
 import constants from '../constants';
 
-// const whiteColor = '#fff';
-// const greyColor = 'grey';
-
 const Button = styled.button.attrs(props => ({
   mainColor: props.color || constants.styles.SECONDARY_COLOR,
   whiteColor: '#fff',
@@ -21,24 +18,23 @@ const Button = styled.button.attrs(props => ({
   font-weight: 600;
   text-align: center;
   border-radius: 5px;
-  color: ${props => (props.outline ? props.mainColor : props.whiteColor)};
-  border: 2px solid ${props => props.mainColor};
-  background-color: ${props =>
-    props.outline ? props.whiteColor : props.mainColor};
+  cursor: pointer;
+  ${props => `
+  color: ${props.outline ? props.mainColor : props.whiteColor};
+  border: 2px solid ${props.mainColor};
+  background-color: ${props.outline ? props.whiteColor : props.mainColor};
 
   :hover {
-    cursor: pointer;
-    color: ${props => (props.outline ? props.whiteColor : props.mainColor)};
-    background-color: ${props =>
-      props.outline ? props.mainColor : props.whiteColor};
+    color: ${props.outline ? props.whiteColor : props.mainColor};
+    background-color: ${props.outline ? props.mainColor : props.whiteColor};
   }
 
   :disabled {
     cursor: default;
-    background-color: ${props =>
-      props.outline ? props.whiteColor : props.greyColor};
-    color: ${props => (props.outline ? props.greyColor : props.whiteColor)};
-    border: 2px solid ${props => props.greyColor};
-  }
+    background-color: ${props.outline ? props.whiteColor : props.greyColor};
+    color: ${props.outline ? props.greyColor : props.whiteColor};
+    border: 2px solid ${props.greyColor};
+  }`}
 `;
+
 export default Button;
--- client/src/components/PageContentAbout/index.js
+++ client/src/components/PageContentAbout/index.js
@@ -3,8 +3,15 @@ import { Helmet } from 'react-helmet';
 // import styled from 'styled-components';
 
 import PageContentWrap from '../PageContentWrap';
+import Button from '../Button';
 
 class PageContentAbout extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { outline: false };
+  }
+
   render() {
     return (
       <PageContentWrap>
@@ -12,9 +19,16 @@ class PageContentAbout extends React.Component {
           <title>About - Study work</title>
         </Helmet>
         <p>This application awesome</p>
+        <Button outline={this.state.outline} onClick={this.handLeButtonClick}>
+          click me!
+        </Button>
       </PageContentWrap>
     );
   }
+
+  handLeButtonClick = () => {
+    this.setState({ outline: !this.state.outline });
+  };
 }
 
 export default PageContentAbout;
